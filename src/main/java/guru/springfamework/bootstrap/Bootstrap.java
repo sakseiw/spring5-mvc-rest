1b320c4f34dae7823ca01638dbce2e1b716e812d
fredo <sakseiw@gmail.com>
2021-03-13 18:41:05 +0100

    adding services

---
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package guru.springfamework.bootstrap;
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRespository;
+
+    public Bootstrap(CategoryRepository categoryRespository) {
+        this.categoryRespository = categoryRespository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRespository.save(fruits);
+        categoryRespository.save(dried);
+        categoryRespository.save(fresh);
+        categoryRespository.save(exotic);
+        categoryRespository.save(nuts);
+
+
+        System.out.println("Data Loaded = " + categoryRespository.count() );
+
+    }
+}
